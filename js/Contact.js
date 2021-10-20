3cc31c02b0ff0e638f9f2c3435267b627308c895
Senthil Kumar S A <sasenthilk@gmail.com>
2021-10-20 05:54:55 +0530

    Modified: To validate form before exporting

--- js/Contact.js
+++ js/Contact.js
@@ -5,7 +5,7 @@ class Contact {
     }
 
     set fullName(fullName) {
-        let nameRegex = RegExp('^[A-Za-z\s]{1,}[\.]{0,1}[A-Za-z\s]{0,}$"')
+        let nameRegex = RegExp('^[A-Z]{1}[A-Za-z\\s]{2,}$');
         if (nameRegex.test(fullName))
             this._fullName = fullName;
         else throw 'Full Name Is Incorrect';
@@ -16,10 +16,15 @@ class Contact {
     }
 
     set address(address) {
-        let cityRegex = RegExp('^[A-Za-z]{4,}$')
-        if (cityRegex.test(address))
-            this._address = address;
-        else throw 'Address Is Incorrect';
+        let addressRegex = RegExp('^(\\b[\\w]{3,}\\s*)+$');
+        var wordCount = address.split(" ").length;
+        if (wordCount > 1) {
+            if (addressRegex.test(address))
+                this._address = address;
+            else
+                throw 'Address Is Incorrect';
+        } else
+            throw 'Address should be of multiple words';
     }
 
     get city() {
@@ -27,10 +32,8 @@ class Contact {
     }
 
     set city(city) {
-        let cityRegex = RegExp('^[A-Za-z]{4,}$')
-        if (cityRegex.test(city))
-            this._city = city;
-        else throw 'City Is Incorrect';
+
+        this._city = city;
     }
 
 
@@ -39,9 +42,7 @@ class Contact {
     }
 
     set state(state) {
-        let stateRegex = RegExp('^[A-Za-z]{4,}$')
-        if (stateRegex.test(state)) this._state = state;
-        else throw 'State Is Incorrect';
+        this._state = state;
     }
 
 
@@ -50,7 +51,7 @@ class Contact {
     }
 
     set zip(zip) {
-        let zipRegex = RegExp('^[0-9]{3}\\s{0,1}[0-9]{3}$')
+        let zipRegex = RegExp('^[0-9]{3}\\s{0,1}[0-9]{3}$');
         if (zipRegex.test(zip)) this._zip = zip;
         else throw 'Zip Is Incorrect';
     }
@@ -60,7 +61,7 @@ class Contact {
     }
 
     set phoneNumber(phoneNumber) {
-        let phoneNumberRegex = RegExp('^[0-9]{10}$')
+        let phoneNumberRegex = RegExp('^[0-9]{10}$');
         if (phoneNumberRegex.test(phoneNumber))
             this._phoneNumber = phoneNumber;
         else throw 'Phone Number Is Incorrect';
@@ -71,7 +72,7 @@ class Contact {
     }
 
     set email(email) {
-        let emailRegex = RegExp('^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$');
+        let emailRegex = RegExp('^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+([\.][a-zA-Z]{2,})+(?:\.[a-zA-Z0-9-]+)*$');
         if (emailRegex.test(email))
             this._email = email;
         else throw 'Email Is Incorrect';
@@ -81,6 +82,6 @@ class Contact {
     toString() {
         return "Full Name = " + this._fullName + ", Address = " + this._address +
             ", City = " + this._city + ", State = " + this._state +
-            ", Zip = " + this._zip + ", Phone Number = " + this._address.phoneNumber + ", Email = " + this._email;
+            ", Zip = " + this._zip + ", Phone Number = " + this._phoneNumber + ", Email = " + this._email;
     }
 }
