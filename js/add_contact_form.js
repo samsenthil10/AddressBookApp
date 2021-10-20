8454244c5f562910a8c2f80719d479b81ca31a79
Senthil Kumar S A <sasenthilk@gmail.com>
2021-10-20 05:42:27 +0530

    Added: To store records to local storage

--- js/add_contact_form.js
+++ js/add_contact_form.js
@@ -1,3 +1,6 @@
+let isUpdate = false;
+let contactObject = {};
+
 window.addEventListener('DOMContentLoaded', (event) => {
 
     print_state("sts");
@@ -18,10 +21,60 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 });
 
-function save() {
+function save(event) {
 
+    event.preventDefault();
+    event.stopPropagation();
+
+    setContactObject();
+    if (site_properties.useLocalStorage.match("true")) {
+        createAndUpdateStorage();
+        resetForm();
+    }
 }
 
 function resetForm() {
 
+}
+
+function setContactObject() {
+    contactObject = new Contact();
+    try {
+        if (!isUpdate && site_properties.useLocalStorage.match("true")) {
+            contactObject.id = createNewContactID();
+        }
+        contactObject.fullName = getInputValueId('#name');
+        contactObject.phoneNumber = getInputValueId('#phone');
+        contactObject.address = getInputValueId('#address');
+        contactObject.city = getInputValueId('.getCity');
+        contactObject.state = getInputValueId('.getState');
+        contactObject.zip = getInputValueId('#zip');
+        alert(contactObject);
+    } catch (e) {
+        throw e;
+    }
+}
+
+function createAndUpdateStorage() {
+    let AddressBookList = getListFromStorage('AddressBookList');
+    if (AddressBookList != undefined) {
+        let addressBookData = AddressBookList
+            .find(contact => contact.id == contactObject.id);
+        if (!addressBookData) AddressBookList.push(contactObject);
+        else {
+            const index = AddressBookList
+                .map(contact => contact.id)
+                .indexOf(addressBookData.id);
+            AddressBookList.splice(index, 1, contactObject);
+        }
+    } else {
+        AddressBookList = [contactObject];
+    }
+    localStorage.setItem("AddressBookList", JSON.stringify(AddressBookList));
+}
+
+function createNewContactID() {
+    let contactID = getListFromStorage('AddressBookList').length;
+    contactID++;
+    return contactID;
 }
